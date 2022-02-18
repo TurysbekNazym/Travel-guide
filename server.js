4fb317bb01b2dcceefb35e450e9c4f329057bfe2
TurysbekNazym <nazym_turysbekova@mail.ru>
2022-02-18 13:52:46 +0600

    fix: changed cli version

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/london'));
+app.use(express.static(__dirname + '/london'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/london/index.html'));
+        '/london/index.html'));
 });
 app.listen(process.env.PORT || 8080);
