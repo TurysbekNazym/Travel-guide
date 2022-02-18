4cab0975759e4d1b8e04554c78a14706f79f6fe8
TurysbekNazym <nazym_turysbekova@mail.ru>
2022-02-18 14:00:18 +0600

    fix: folder url

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/london'));
+app.use(express.static(__dirname + '/dist/London'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/london/index.html'));
+        '/dist/London/index.html'));
 });
 app.listen(process.env.PORT || 8080);
